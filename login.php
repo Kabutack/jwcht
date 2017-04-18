772bbfd4b3ac898f02f63a799c1714c1c88924ed
huangguofeng <hgf0210@gmail.com>
2017-04-18 18:06:57 +0800

    save

--- login.php
+++ login.php
@@ -6,9 +6,20 @@
 	
 	$config = include '/vendor/yuan1994/z-crawler/config.php';
 
-	//获取学生学号&密码
+	//获取学生学号&密码&验证码
 	$username = $_GET['username'];
 	$password = $_GET['password'];
+	$codenum = $_GET['codenum'];
+
 	// 实例化
 	$zCrawler = new ZCrawler($username, $password, $config);
+
+	//获取实例
+	$login = $zCrawler->login;
+
+	//使用验证码登录
+	$cookie = $login->withCode($codenum);//返回GuzzleHttp\Cookie\CookieJar Cookie 对象
+
+	echo $cookie;
+
 ?>
